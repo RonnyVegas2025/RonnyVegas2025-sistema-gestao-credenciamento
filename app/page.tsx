e542685658cc55f01de592929c0322f50442e4a9
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-05-04 16:08:02 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Sistema de Gestão de Credenciamento</h1>
+      <p>Projeto rodando na Vercel 🚀</p>
+    </div>
+  );
+}
